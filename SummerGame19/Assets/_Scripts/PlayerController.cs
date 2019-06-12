d6ab297362f2b2efb6fccbb0971e88f61f4bb1ea
Meloonius <48081701+meloonius@users.noreply.github.com>
2019-06-12 11:17:37 +0300

    update

---
+++ SummerGame19/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    Rigidbody2D rigBody;
+    Vector2 moveVelocity;
+    Collider2D boxCol;
+    void Start()
+    {
+        rigBody = GetComponent<Rigidbody2D>();
+        boxCol = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+    }
+
+
+    private void FixedUpdate()
+    {
+        rigBody.MovePosition(rigBody.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}

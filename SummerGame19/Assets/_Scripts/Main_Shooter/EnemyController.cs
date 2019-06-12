97a9e303c56462b7dc78e4763f33d1c0de765a59
Meloonius <48081701+meloonius@users.noreply.github.com>
2019-06-12 12:55:47 +0300

    update

---
+++ SummerGame19/Assets/_Scripts/Main_Shooter/EnemyController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public float accelerationTime = 2f;
+    public float maxSpeed = 5f;
+    private Vector2 movement;
+    private float timeLeft;
+
+    public Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0)
+        {
+            movement = new Vector2(Random.Range(-1f, 1f), Random.Range(0f, 0f));
+            timeLeft += accelerationTime;
+        }
+
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Edge"))
+        {
+            movement.x *= -1f;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(movement * maxSpeed);
+    }
+
+}

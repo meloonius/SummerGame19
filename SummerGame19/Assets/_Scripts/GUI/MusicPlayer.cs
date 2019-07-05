cd45cdeb1e1418d9efca8687adbc39ed09383713
Meloonius <48081701+meloonius@users.noreply.github.com>
2019-07-05 11:36:16 +0300

    Music player added

---
+++ SummerGame19/Assets/_Scripts/GUI/MusicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+    static bool AudioBegin = false;
+    public AudioSource gameMusic;
+
+    void Awake()
+    {
+        if (!AudioBegin)
+        {
+            gameMusic = GetComponent<AudioSource>();
+            DontDestroyOnLoad(gameObject);
+            AudioBegin = true;
+        }
+    }
+}
